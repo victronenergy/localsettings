a107059b95c760adc91cfb6b95d9c01cc081b8f7
Arnoud Velema <avelema@victronenergy.com>
2013-02-28 11:52:30 +0000

    add AddSetting functionality

--- localsettings.py
+++ localsettings.py
@@ -32,10 +32,12 @@ logPath = ''
 
 ## The dbus object
 bus = None
+busName = None
 
 ## Dbus service name
 dbusName = 'com.victronenergy.settings'
 InterfaceBusItem = 'com.victronenergy.BusItem'
+InterfaceSettings = 'com.victronenergy.Settings'
 
 ## The dictonary's
 settings = {}
@@ -51,6 +53,7 @@ timeoutSaveSettingsEventId = None
 timeoutSaveSettingsTime = 5
 execPath = ''
 fileSettings = ''
+fileDefaults = ''
 
 class MyDbusObject(dbus.service.Object):
 	global InterfaceBusItem
@@ -58,6 +61,10 @@ class MyDbusObject(dbus.service.Object):
 	def __init__(self, busName, objectPath):
 		dbus.service.Object.__init__(self, busName, objectPath)
 
+	@dbus.service.method(InterfaceBusItem, in_signature = 'si', out_signature = 's')
+	def GetDescription(self, language, length):
+		return 'invalid'
+	
 	@dbus.service.method(InterfaceBusItem, out_signature = 'v')
 	def GetValue(self):
 		global settings
@@ -82,16 +89,20 @@ class MyDbusObject(dbus.service.Object):
 
 	@dbus.service.signal(InterfaceBusItem, signature = 'v')
 	def _setValue(self, value):
-		global timeoutSaveSettingsEventId
-		global timeoutSaveSettingsTime
 		global settings
 
-		tracing.log.info('_setValue %s %s' % (self._object_path, value))
+		tracing.log.debug('_setValue %s %s' % (self._object_path, value))
 		settings[self._object_path] = value
+		self._startTimeoutSaveSettings(True, False)
+
+	def _startTimeoutSaveSettings(self, writeSettings, writeDefaults):
+		global timeoutSaveSettingsEventId
+		global timeoutSaveSettingsTime
+
 		if timeoutSaveSettingsEventId:
 			source_remove(timeoutSaveSettingsEventId)
 			timeoutSaveSettingsEventId = None
-		timeoutSaveSettingsEventId = timeout_add(timeoutSaveSettingsTime*1000, saveSettings)
+		timeoutSaveSettingsEventId = timeout_add(timeoutSaveSettingsTime*1000, saveSettings, writeSettings, writeDefaults)
 
 	@dbus.service.method(InterfaceBusItem, out_signature = 'v')
 	def GetDefault(self):
@@ -113,21 +124,57 @@ class MyDbusObject(dbus.service.Object):
 			self._setValue(defaults[self._object_path])
 		return 0
 
-def saveSettings():
+	@dbus.service.method(InterfaceSettings, in_signature = 'ssv', out_signature = 'i')
+	def AddSetting(self, group, name, defaultValue):
+		global groups
+		global settings
+		global defaults
+		global busName
+		global myDbusGroupServices
+		global myDbusServices
+
+		if self._object_path in groups:
+			pathGroup = self._object_path + '/' + str(group)
+			pathItem = pathGroup + '/' + str(name)
+			tracing.log.debug('Add %s %s' % (pathItem, defaultValue))
+			if not pathGroup in groups:
+				groups.append(pathGroup)
+				myDbusObject = MyDbusObject(busName, pathGroup)
+				myDbusGroupServices.append(myDbusObject)
+			if not pathItem in settings:
+				settings[pathItem] = str(defaultValue)
+				defaults[pathItem] = str(defaultValue)
+				tracing.log.debug(settings.items())
+				tracing.log.debug(defaults.items())
+				tracing.log.debug(groups)
+				self._startTimeoutSaveSettings(True, True)
+				myDbusObject = MyDbusObject(busName, pathItem)
+				myDbusServices.append(myDbusObject)
+				return 0
+			else:
+				return -1
+		else:
+			return -1
+
+def saveSettings(writeSettings, writeDefaults):
 	global timeoutSaveSettingsEventId
 	global settings
 	global fileSettings
+	global fileDefaults
 
-	tracing.log.info('saveSettings')
+	tracing.log.debug('saveSettings')
 	source_remove(timeoutSaveSettingsEventId)
 	timeoutSaveSettingsEventId = None
-	parseDictonaryToXmlFile(settings, fileSettings)
+	if writeSettings:
+		parseDictonaryToXmlFile(settings, fileSettings)
+	if writeDefaults:
+		parseDictonaryToXmlFile(defaults, fileDefaults)
 
 def parseXmlFileToDictonary(file, dictonaryItems, arrayGroups):
 	parser = etree.XMLParser(remove_blank_text=True)
 	tree = etree.parse(file, parser)
 	root = tree.getroot()
-	tracing.log.info(etree.tostring(root))
+	tracing.log.debug(etree.tostring(root))
 	parseXmlToDictonary(root, '/', dictonaryItems, arrayGroups)
 
 def parseXmlToDictonary(element, path, dictonaryItems, arrayGroups):
@@ -139,10 +186,12 @@ def parseXmlToDictonary(element, path, dictonaryItems, arrayGroups):
 
 	if element.text:
 		dictonaryItems[path] = element.text
-	elif arrayGroups != None:		
-		arrayGroups.append(path)
+	elif arrayGroups != None:
+		if not path in arrayGroups:
+			arrayGroups.append(path)
 
 def parseDictonaryToXmlFile(dictonary, file):
+	tracing.log.debug('parseDictonaryToXmlFile %s' % file)
 	root = None
 	for key in list(dictonary):
 		items = key.split('/')
@@ -153,7 +202,7 @@ def parseDictonaryToXmlFile(dictonary, file):
 		items.remove(root.tag)
 		elem = root
 		for item in items:
-			foundElem = root.find(item)
+			foundElem = elem.find(item)
 			if foundElem == None:
 				elem = etree.SubElement(elem, item)
 			else:
@@ -167,7 +216,7 @@ def parseDictonaryToXmlFile(dictonary, file):
 # @param signum the signal-number.
 # @param stack the call-stack.
 def handlerSignals(signum, stack):
-	tracing.log.info('handlerSignals received: %d' % signum)
+	tracing.log.debug('handlerSignals received: %d' % signum)
 	exitCode = 0
 	if signum == signal.SIGHUP:
 		exitCode = 1
@@ -182,7 +231,9 @@ def run():
 	global settings
 	global execPath
 	global fileSettings
+	global fileDefaults
 	global groups
+	global busName
 
 	DBusGMainLoop(set_as_default=True)
 
@@ -193,7 +244,7 @@ def run():
 
 	# setup debug traces.
 	tracing.setupDebugTraces(execPath)
-	tracing.log.info('tracingPath = %s' % execPath)
+	tracing.log.debug('tracingPath = %s' % execPath)
 
 	# Print the logscript version
 	tracing.log.info('Localsettings version is: 0x%04x' % version)
@@ -201,7 +252,7 @@ def run():
 	
 	# Trace the python version.
 	pythonVersion = platform.python_version()
-	tracing.log.info('Current python version: %s' % pythonVersion)
+	tracing.log.debug('Current python version: %s' % pythonVersion)
 
 	# setup signal handling.
 	signal.signal(signal.SIGHUP, handlerSignals) # 1: Hangup detected
@@ -211,18 +262,18 @@ def run():
 
 	# read the settings.xml and defaults.xml
 	parseXmlFileToDictonary(fileSettings, settings, groups)
-	tracing.log.info(settings.items())
-	tracing.log.info(groups)
+	tracing.log.debug(settings.items())
+	tracing.log.debug(groups)
 	parseXmlFileToDictonary(fileDefaults, defaults, None)
-	tracing.log.info(defaults.items())
+	tracing.log.debug(defaults.items())
 
 	# get on the bus
 	if isHostPC():
 		bus = dbus.SessionBus()
-		tracing.log.info('SessionBus')
+		tracing.log.debug('SessionBus')
 	else:
 		bus = dbus.SystemBus()
-		tracing.log.info('SystemBus')
+		tracing.log.debug('SystemBus')
 	busName = dbus.service.BusName(dbusName, bus)
 	for setting in settings:
 		myDbusObject = MyDbusObject(busName, setting)
