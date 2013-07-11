18b6cddf1a4e3a5d10ce357c188aa5b01333dda4
Arnoud Velema <avelema@victronenergy.com>
2013-07-11 14:32:22 +0000

    change version to 0x0004

--- localsettings.py
+++ localsettings.py
@@ -47,7 +47,7 @@ import platform
 ## Major version.
 FIRMWARE_VERSION_MAJOR = 0x00
 ## Minor version.
-FIRMWARE_VERSION_MINOR = 0x03
+FIRMWARE_VERSION_MINOR = 0x04
 ## Localsettings version.
 version = (FIRMWARE_VERSION_MAJOR << 8) | FIRMWARE_VERSION_MINOR
 
