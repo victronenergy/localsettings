91e0511bcdae6132676d1f02175f416680a79f37
mpvader <mvader@victronenergy.com>
2016-08-22 23:10:28 +0200

    tests: improve coverage of the tests
    - add test of adding a float
    - add test of adding values with min max
    - add testing all properties of a setting, instead of only the value

--- test/test_localsettings.py
+++ test/test_localsettings.py
@@ -13,6 +13,7 @@ import platform
 import dbus
 import threading
 import fcntl
+import copy
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 
@@ -26,7 +27,11 @@ logger = logging.getLogger(__file__)
 class LocalSettingsTest(unittest.TestCase):
 	def setUp(self):
 		# Always start with a fresh and running instance of localsettings
-		os.remove('/data/conf/settings.xml')
+		try:
+			os.remove('/data/conf/settings.xml')
+		except OSError:
+			pass
+
 		self._startLocalSettings()
 		self._dbus = dbus.SystemBus() if (platform.machine() == 'armv7l') else dbus.SessionBus()
 
@@ -61,15 +66,26 @@ class LocalSettingsTest(unittest.TestCase):
 		self.assertEqual(self._called, ['com.victronenergy.settings', '/Settings/' + details['group'] + '/' + details['setting'], {'Text': '100', 'Value': 100}])
 
 	def test_adding_new_settings_and_readback(self):
-		testsets = {
-			'int-no-min-max': {'group': 'g', 'setting': 's', 'value': 100, 'type': 'i', 'min': 0, 'max': 0},
-			'start-group-with-digit': {'group': '0g', 'setting': 's', 'value': 100, 'type': 'i', 'min': 0, 'max': 0},
-			'start-setting-with-digit': {'group': 'g', 'setting': '0s', 'value': 100, 'type': 'i', 'min': 0, 'max': 0}
-		}
+		from collections import OrderedDict
+		testsets = OrderedDict()
+		testsets['int-no-min-max'] = {'group': 'g', 'setting': 'in', 'default': 100, 'value': 100, 'type': 'i', 'min': 0, 'max': 0}
+		testsets['int-with-min-max'] =  {'group': 'g', 'setting': 'iw', 'default': 101, 'value': 101, 'type': 'i', 'min': 0, 'max': 101}
+
+		# float-no-min-max doesn't work, because localsettings does not recognize 0.0 and 0.0 as no min max, only 0 and 0 works.
+		# testsets['float-no-min-max'] = {'group': 'g', 'setting': 'f', 'default': 102.0, 'value': 102.0, 'type': 'f', 'min': 0.0, 'max': 0.0}
+
+		testsets['float-with-min-max'] = {'group': 'g', 'setting': 'f', 'default': 103.0, 'value': 103.0, 'type': 'f', 'min': 0.0, 'max': 1000.0}
+		testsets['start-group-with-digit'] = {'group': '0g', 'setting': 's', 'default': 104, 'value': 104, 'type': 'i', 'min': 0, 'max': 0}
+		testsets['start-setting-with-digit'] = {'group': 'g', 'setting': '0s', 'default': 105, 'value': 105, 'type': 'i', 'min': 0, 'max': 0}
+
+		# re-adding a setting results in an error. See open issue on github.
+		# testsets['int-re-add-same-min-max'] = {'group': 'g', 'setting': 'in', 'default': 200, 'value': 100, 'type': 'i', 'min': 0, 'max': 0}
+		# testsets['int-re-add-other-min-max'] = {'group': 'g', 'setting': 'in', 'default': 201, 'value': 100, 'type': 'i', 'min': 0, 'max': 1000}
+
 
 		for name, details in testsets.iteritems():
 			print "\n\n===Testing %s===\n" % name
-			self.assertEqual(0, self._add_setting(details['group'], details['setting'], details['value'], details['type'], details['min'], details['max']))
+			self.assertEqual(0, self._add_setting(details['group'], details['setting'], details['default'], details['type'], details['min'], details['max']))
 
 			# wait 2.5 seconds, since local settings itself waits 2 seconds before it stores the data.
 			time.sleep(2.5)
@@ -79,8 +95,25 @@ class LocalSettingsTest(unittest.TestCase):
 			time.sleep(2)
 			self._startLocalSettings()
 
-			self.assertEqual(VeDbusItemImport(self._dbus, 'com.victronenergy.settings',
-				'/Settings/' + details['group'] + '/' + details['setting']).get_value(), details['value'])
+			# read the results
+			i = VeDbusItemImport(
+				self._dbus,
+				'com.victronenergy.settings',
+				'/Settings/' + details['group'] + '/' + details['setting'],
+				eventCallback=None,
+				createsignal=False)
+			result = copy.deepcopy(details)
+			result['value'] = i.get_value().real
+			result['default'] = i._proxy.GetDefault().real
+
+			# don't ask me why, but getMin() and getMax() return a string...
+			result['min'] = int(i._proxy.GetMin()) if details['type'] == 'i' else float(i._proxy.GetMin())
+			result['max'] = int(i._proxy.GetMax()) if details['type'] == 'i' else float(i._proxy.GetMax())
+
+			# don't check the type, as there is no GetType() available
+			# result['type'] = i._proxy.GetType()
+
+			self.assertEqual(details, result)
 
 	def test_adding_new_settings_with_underscore_fails(self):
 		testsets = {
