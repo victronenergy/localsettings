b3ea12f0c7baad100922cb0188d7e33e5a8f3797
Mans Rullgard <mans@mansr.com>
2017-11-06 16:36:11 +0000

    migrate canbus settings on startup

--- localsettings.py
+++ localsettings.py
@@ -560,6 +560,64 @@ def handlerSignals(signum, stack):
 		exitCode = 1
 	exit(exitCode)
 
+# Migration code.
+# TODO ideally this should be elsewhere.
+def delete_from_tree(tree, path):
+	obj = tree.xpath(path)
+	if not obj:
+		return
+	obj[0].getparent().remove(obj[0])
+
+## Migrate old canbus settings
+def migrate_can_profile(tree):
+	global fileSettings
+	global newFileSettings
+
+	if not os.path.isfile("/etc/venus/canbus_ports"):
+		return
+
+	with open('/etc/venus/canbus_ports', 'r') as f:
+		interface = f.readline().split(None, 1)[0]
+
+	path = "/Settings/Canbus/" + interface + "/Profile"
+
+	if tree.xpath(path):
+		return
+
+	# default to Ve.Can
+	profile = 1
+
+	if tree.xpath("/Settings/Services/LgResu/text()") == ["1"]:
+		profile = 3
+	elif tree.xpath("/Settings/Services/OceanvoltMotorDrive/text()") == ["1"] or \
+		tree.xpath("/Settings/Services/OceanvoltValence/text()") == ["1"]:
+		profile = 4
+	elif tree.xpath("/Settings/Services/VeCan/text()") == ["0"]:
+		profile = 0
+
+	print("Setting " + path + " to " + str(profile))
+
+	settings = tree.getroot()
+	canbus = settings.find("Canbus")
+	if canbus == None:
+		canbus = etree.SubElement(settings, "Canbus")
+
+	inter = canbus.find(interface)
+	if inter == None:
+		inter = etree.SubElement(canbus, interface)
+
+	prof = etree.SubElement(inter, "Profile")
+	prof.text = str(profile)
+	prof.set('type', 'i')
+
+	delete_from_tree(tree, "/Settings/Services/LgResu")
+	delete_from_tree(tree, "/Settings/Services/OceanvoltMotorDrive")
+	delete_from_tree(tree, "/Settings/Services/OceanvoltValence")
+	delete_from_tree(tree, "/Settings/Services/VeCan")
+
+	tree.write(newFileSettings, pretty_print = True, xml_declaration = True, encoding = settingsEncoding)
+	rename(newFileSettings, fileSettings)
+
 ## The main function.
 def run():
 	global bus
@@ -628,6 +686,7 @@ def run():
 		try:
 			tree = etree.parse(fileSettings)
 			root = tree.getroot()
+			migrate_can_profile(tree)
 			tracing.log.info('Settings file %s validated' % fileSettings)
 		except:
 			tracing.log.error('Settings file %s invalid' % fileSettings)
