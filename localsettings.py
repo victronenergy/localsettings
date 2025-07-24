c308ae19e034a9ab9fb7399ad39f35199eb9fbb7
Izak Burger <iburger@victronenergy.com>
2025-07-24 16:08:38 +0200

    v1.68

--- localsettings.py
+++ localsettings.py
@@ -47,7 +47,7 @@ from gi.repository import GLib
 ## Major version.
 FIRMWARE_VERSION_MAJOR = 0x01
 ## Minor version.
-FIRMWARE_VERSION_MINOR = 0x67
+FIRMWARE_VERSION_MINOR = 0x68
 ## Localsettings version.
 version = (FIRMWARE_VERSION_MAJOR << 8) | FIRMWARE_VERSION_MINOR
 
