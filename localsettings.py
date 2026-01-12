3f81e3cf5ac1021e92029f0edb8734aa83a0ce00
Jeroen Hofstee <jhofstee@victronenergy.com>
2026-01-12 14:48:28 +0100

    v1.72

--- localsettings.py
+++ localsettings.py
@@ -50,7 +50,7 @@ from gi.repository import GLib
 ## Major version.
 FIRMWARE_VERSION_MAJOR = 0x01
 ## Minor version.
-FIRMWARE_VERSION_MINOR = 0x71
+FIRMWARE_VERSION_MINOR = 0x72
 ## Localsettings version.
 version = (FIRMWARE_VERSION_MAJOR << 8) | FIRMWARE_VERSION_MINOR
 
